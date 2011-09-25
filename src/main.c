8e77fe6f160372b89efbaed8c3c04245891b32d7
Michael Moorman <tensorpuddin@devio.us>
2011-09-25 19:21:44 -0400

    more updates

--- src/main.c
+++ src/main.c
@@ -46,14 +46,19 @@ set_timer (GtkButton* button,
     {
       mult = 1;
     }
+  else
+    {
+      mult = 1;
+    }
 
   x = gtk_spin_button_get_value_as_int (c->time_entry);
   
-  g_timeout_add (x*mult, run_alarm, c);
+  //  g_timeout_add (x*mult, run_alarm, c);
+  g_print ("The seconds to wait is %d\n", x*mult);
 
   gtk_widget_destroy (GTK_WIDGET (c->countdown));
-  g_free(cunits);
-  g_free(units);
+  //  g_free(cunits);
+  //  g_free(units);
 }
 
 
@@ -61,34 +66,39 @@ int
 main (int argc, char *argv[])
 {
   Countdown  *c;
-  GtkBuilder builder;
 
   gtk_init (&argc, &argv);
   c = g_new(Countdown, 1);
   c->builder = gtk_builder_new ();
   gtk_builder_add_from_file (c->builder, "./countdown.ui", NULL);
   
-  c->countdown = gtk_builder_get_object (c->builder, "countdown");
-  g_signal_connect (c->countdown, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+  c->countdown = GTK_WINDOW (gtk_builder_get_object (c->builder,
+                                                     "countdown"));
+  g_signal_connect (c->countdown, "destroy",
+                    G_CALLBACK (gtk_main_quit), NULL);
 
-  c->unit_box = gtk_builder_get_object (c->builder, "unit_box");
+  c->unit_box = GTK_COMBO_BOX_TEXT (gtk_builder_get_object (c->builder,
+                                                            "unit_box"));
 
-  c->time_entry = gtk_builder_get_object (c->builder, "time_entry");
+  c->time_entry = GTK_SPIN_BUTTON (gtk_builder_get_object (c->builder,
+                                                           "time_entry"));
 
   //  c->about_button = gtk_builder_get_object (builder, "about_button");
   //  g_signal_connect (c->about_button, "clicked", G_CALLBACK (on_about_clicked), NULL);
 
-  c->cancel_button = gtk_builder_get_object (c->builder,
-                                             "cancel_button");
+  c->cancel_button = GTK_BUTTON (gtk_builder_get_object (c->builder,
+                                                         "cancel_button"));
   g_signal_connect (c->cancel_button, "clicked",
                     G_CALLBACK (gtk_main_quit), NULL);
 
-  c->set_timer_button = gtk_builder_get_object (c->builder,
-                                                "set_timer_button");
+  c->set_timer_button = GTK_BUTTON (gtk_builder_get_object (c->builder,
+                                                            "set_timer_button"));
   g_signal_connect (c->set_timer_button, "clicked",
                     G_CALLBACK (set_timer), (gpointer)c);
 
+
+  gtk_widget_show (GTK_WIDGET (c->countdown));
   gtk_main ();
-  g_free((gpointer)c);
+  //  g_free((gpointer)c);
   return 0;
 }
--- src/main.h
+++ src/main.h
@@ -1,6 +1,4 @@
 #ifndef COUNTDOWN_MAIN_H
 #define COUNTDOWN_MAIN_H
 
-static gboolean on_button_clicked (GtkWidget*,GdkEvent*,gpointer);
-
 #endif // COUNTDOWN_MAIN_H
